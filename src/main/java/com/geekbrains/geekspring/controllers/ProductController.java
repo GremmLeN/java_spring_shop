51f31311a1cd8cbd2d0f403a2ad561303be01ee5
GremmLeN <GremmLeN@gmail.com>
2021-03-10 20:28:32 +0500

    new base project L2

--- src/main/java/com/geekbrains/geekspring/controllers/ProductController.java
+++ src/main/java/com/geekbrains/geekspring/controllers/ProductController.java
@@ -1,34 +1,74 @@
 package com.geekbrains.geekspring.controllers;
 
 import com.geekbrains.geekspring.entities.Product;
-import com.geekbrains.geekspring.entities.Student;
+import com.geekbrains.geekspring.entities.ProductImage;
+import com.geekbrains.geekspring.services.CategoryService;
+import com.geekbrains.geekspring.services.ImageSaverService;
 import com.geekbrains.geekspring.services.ProductService;
-import com.geekbrains.geekspring.services.StudentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.transaction.Transactional;
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/products")
-@Transactional
 public class ProductController {
     private ProductService productService;
+    private CategoryService categoryService;
+    private ImageSaverService imageSaverService;
 
     @Autowired
     public void setProductService(ProductService productService) {
         this.productService = productService;
     }
 
-    @RequestMapping("/list")
-    @Transactional
-    public String showStudentsList(Model model) {
-        List<Product> allProduct = productService.getAllProducts();
-        model.addAttribute("productsList", allProduct);
-        return "products-list";
+    @Autowired
+    public void setCategoryService(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
+    @Autowired
+    public void setImageSaverService(ImageSaverService imageSaverService) {
+        this.imageSaverService = imageSaverService;
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable(name = "id") Long id) {
+        Product product = productService.getProductById(id);
+        if (product == null) {
+            product = new Product();
+            product.setId(0L);
+        }
+        model.addAttribute("product", product);
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "/edit-product";
+    }
+
+    @PostMapping("/edit")
+    public String processProductAddForm(@Valid @ModelAttribute("product") Product product, BindingResult theBindingResult, Model model, @RequestParam("file") MultipartFile file) {
+        if (product.getId() == 0 && productService.isProductWithTitleExists(product.getTitle())) {
+            theBindingResult.addError(new ObjectError("product.title", "Товар с таким названием уже существует")); // todo не отображает сообщение
+        }
+
+        if (theBindingResult.hasErrors()) {
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "edit-product";
+        }
+
+        if (!file.isEmpty()) {
+            String pathToSavedImage = imageSaverService.saveFile(file);
+            ProductImage productImage = new ProductImage();
+            productImage.setPath(pathToSavedImage);
+            productImage.setProduct(product);
+            product.addImage(productImage);
+        }
+
+        productService.saveProduct(product);
+        return "redirect:/shop";
+    }
 }
